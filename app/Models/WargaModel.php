f5f0881df8609b95ab6bab29dbf08fdd4238cfd2
irham <i.maulana.azn@gmail.com>
2025-11-12 21:41:53 +0700

    initial version of this app

---
+++ app/Models/WargaModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class WargaModel extends Model
+{
+
+    protected $table            = 'warga';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+
+    protected $allowedFields    = [
+        'nik',
+        'nama_lengkap',
+        'alamat',
+        'no_hp',
+        'email',
+        'jenis_kelamin',
+        'agama',
+        'status',
+        'foto',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $useTimestamps    = true;
+    protected $createdField     = 'created_at';
+    protected $updatedField     = 'updated_at';
+}
